4ba36b72fb0a828759d6bafdab1a1ff8d373e648
Furkan Hanci <furkan@elephantapps.co>
2022-01-10 06:39:02 +0300

    background color changed

---
+++ QuakeX/QuakeX/Support/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  QuakeX
+//
+//  Created by Furkan Hanci on 1/3/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let url = "https://api.orhanaydogdu.com.tr/deprem/live.php?limit=100"
+    static let baseUrl = "https://api.orhanaydogdu.com.tr"
+    
+    
+}
